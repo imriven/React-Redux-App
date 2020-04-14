9a3588e336f4e4716c596bc99f3a71c64655f5c3
Christina Harris <christinathedesigner@outlook.com>
2020-04-14 15:48:27 -0700

    added in styling

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,55 @@ import './App.css';
 import { useSelector, useDispatch } from "react-redux"
 import { getMovies } from "./actions"
 import Movie from "./components/movie"
+import  Video  from "./media/background1.mp4"
+import Styled from "styled-components"
 
+/** Styles **/
+
+const BackgroundContainer = Styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+margin-top: 5%;
+border-radius: 0 5%;
+padding: 1%;
+width: 60%;
+color:white;
+text-align: center;
+`
+
+const ContainerDiv = Styled.div`
+display: flex;
+flex-wrap: wrap;
+align-content:center;
+justify-content: center;
+margin: 3% 0 5% 0;
+width: 100%;
+
+`
+
+const VideoBackground = Styled.div`
+display: flex;
+justify-content: center;
+`
+
+const AwesomeHeader = Styled.h1`
+display:flex;
+text-align:center;
+color: white;
+text-shadow: 5px 5px 8px mediumPurple;
+text-align: center;
+`
+
+const AwesomeTitleHeader = Styled.h2`
+display:flex;
+text-align:center;
+color: white;
+text-shadow: 5px 5px 8px lime;
+text-align: center;
+`
+
+/***/
 function App() {
   const movies = useSelector(state => state.movies)
   const isFetching = useSelector(state => state.isFetching)
@@ -14,11 +62,19 @@ function App() {
     dispatch(getMovies())
   }, [getMovies])
   return (
-    <div>
+    <VideoBackground>
+      <video style={{ zIndex: -1, position: "fixed", minWidth: "100%", minHeight: "100vh" }} autoPlay loop muted>
+          <source src={Video} type="video/mp4" />
+          </video>
+          <BackgroundContainer>
+            <AwesomeHeader>Studio Ghibli Movies</AwesomeHeader>
+            <ContainerDiv>
       {isFetching && <p>Fetching Movies...</p>}
      {movies && movies.map(movie => <Movie movie={movie}/>)}
   {error && <p>{error}</p>}
-    </div>
+  </ContainerDiv>
+  </BackgroundContainer>
+    </VideoBackground>
   );
 }
 
--- app/src/components/movie.js
+++ app/src/components/movie.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import Styled from "styled-components"
+
+
+const ContainerDiv = Styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content:center;
+align-items:center;
+margin: 5%;
+padding: 3%;
+width: 25%;
+color:white;
+background: linear-gradient(90deg, rgba(185,157,231,0.6222864145658263) 0%, rgba(101,171,78,0.6923144257703081) 47%, rgba(185,157,231,0.6222864145658263) 95%);
+border-radius: 25px;
+`
+
+const AwesomeHeader = Styled.h3`
+color: white;
+margin: 0 5%;
+`
+
 
 
 function Movie(props) {
   return (
-    <div >
-        <p>Title: {props.movie.title}</p>
+    <ContainerDiv>
+        <AwesomeHeader>Title: {props.movie.title}</AwesomeHeader>
         <p>Director: {props.movie.director}</p>
   <p>Description: {props.movie.description}</p>
-    </div>
+    </ContainerDiv>
   );
 }
 
